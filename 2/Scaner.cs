289e3d62740c144479b7415bad109b71bfbe2f74
jakub.temr <jakub.temr@atreo.cz>
2024-03-12 12:05:06 +0100

    feat: add

---
+++ 2/Scaner.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    public class Scaner
+    {
+        private StreamReader file;
+        private int currentChar;
+        public Scaner(StreamReader file)
+        {
+            this.file = file;
+            this.currentChar = file.Read();
+        }
+
+        public Token NextToken()
+        {
+            while (char.IsWhiteSpace((char)currentChar) || currentChar == '/')
+            {
+                if (currentChar == '/')
+                {
+                    NextChar();
+                    if (currentChar == '/')
+                    {
+                        while (currentChar != '\n' && currentChar != -1)
+                        {
+                            NextChar();
+                        }
+                    }
+                }
+                NextChar();
+            }
+
+            if (currentChar == -1)
+            {
+                return new Token(TokenType.EOF, "");
+            }
+
+            if (char.IsLetter((char)currentChar))
+            {
+                StringBuilder identifier = new StringBuilder();
+                while (char.IsLetterOrDigit((char)currentChar))
+                {
+                    identifier.Append((char)currentChar);
+                    NextChar();
+                }
+
+                string value = identifier.ToString();
+                TokenType type = value switch
+                {
+                    "div" => TokenType.DIV,
+                    "mod" => TokenType.MOD,
+                    _ => TokenType.ID,
+                };
+
+                return new Token(type, value);
+            }
+
+            if (char.IsDigit((char)currentChar))
+            {
+                StringBuilder number = new StringBuilder();
+                while (char.IsDigit((char)currentChar))
+                {
+                    number.Append((char)currentChar);
+                    NextChar();
+                }
+
+                return new Token(TokenType.NUM, number.ToString());
+            }
+            Token? token = currentChar switch
+            {
+                '+' or '-' or '*' or '/' => new Token(TokenType.OP, ((char)currentChar).ToString()),
+                '(' => new Token(TokenType.LPAR, "("),
+                ')' => new Token(TokenType.RPAR, ")"),
+                ';' => new Token(TokenType.SEMICOLON, ";"),
+                _ => null
+            };
+            NextChar();
+
+            return token;
+        }
+
+        private void NextChar()
+        {
+            currentChar = file.Read();
+        }
+    }
+}
---
+++ 2/Token.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    public enum TokenType
+    {
+        EOF,
+        OP,
+        NUM,
+        LPAR,
+        DIV,
+        RPAR,
+        SEMICOLON,
+        MOD,
+        ID
+    }
+    public class Token
+    {
+        public TokenType Type;
+        public string Value;
+        public Token(TokenType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            switch (Type)
+            {
+                case TokenType.LPAR:
+                case TokenType.RPAR:
+                case TokenType.SEMICOLON:
+                case TokenType.MOD:
+                case TokenType.DIV:
+                    return Type.ToString();
+
+                default:
+                    return $"{Type}: {Value}";
+            }
+        }
+    }
+}
